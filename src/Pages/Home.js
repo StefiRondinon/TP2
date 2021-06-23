3b37fd8e4727fdf76bfd472c70eea48d098af587
Stefania Rondinon <rondinonstefi@gmail.com>
2021-06-23 18:02:41 -0300

    commit2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,37 +1,86 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 import Producto from '../Components/Producto';
+import ProductoP from '../Components/Producto';
 
+function Home(props){
 
-class Home extends Component {
-    constructor() {
-        super()
-        this.state = {
-            producto: []
-        }
-    }
+    const [loading, setLoading] = useState(true);
+    const [producto, setProducto] = useState({})
 
-    componentDidMount(prevProps, prevState) {
-        fetch("https://jsonfy.com/items")
-            .then(res => res.json())
-            .then(data => {
-                this.setState({
-                    producto: data
+    useEffect(
+        () => {
+            fetch("https://jsonfy.com/items")
+                .then(res => res.json())
+                .then(data => {
+                    console.log("data", data)
+                    setLoading(false);
+                    setProducto(data[0]);
                 })
-            })
-    }
+        },
+        []
+    )
 
-    render() {
-
+    if (loading) {
         return (
-            <div className="productos">
-                <div className="contenedor-productos">
-                    {this.state.producto.map(items=> <Producto datos={items} />)}
-                </div>
+            <div>
+                Loading ...
             </div>
         )
+    } else {
+        return (
+            <div>{Producto}</div>
+            )
     }
+    // return (
+    //     <div className="productos">
+    //         <div className="contenedor-productos">
+    //             {setProducto(items => <Producto datos={items} />)}
+    //         </div>
+    //     </div>
+    // )
 }
+
+{/* <div className="productos">
+<div className="contenedor-productos">
+    <h1>{producto.name}</h1>
+    <h2>{producto.description}</h2>
+    <h3>{producto.price}</h3>
+    <img className="imagen" src={producto.photo_url}></img>
+</div>
+</div> */}
+
+
+
+// class Home extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             producto: []
+//         }
+//     }
+
+//     componentDidMount(prevProps, prevState) {
+//         fetch("https://jsonfy.com/items")
+//             .then(res => res.json())
+//             .then(data => {
+//                 this.setState({
+//                     producto: data
+//                 })
+//             })
+//     }
+
+//     render() {
+
+//         return (
+//             <div className="productos">
+//                 <div className="contenedor-productos">
+//                     {this.state.producto.map(items=> <Producto datos={items} />)}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 
 
